77aa649cc5a5e684ac457906a61976df6d7553fe
boernsen-development <77436741+boernsen-development@users.noreply.github.com>
2022-05-02 17:48:10 +0200

    more TODOs

--- control.lua
+++ control.lua
@@ -1,3 +1,5 @@
+-- TODO change git username / email in old commits
+-- TODO add doc for TTL texts (README.md?)
 -- TODO add "back on path" accouncement for state change no_path/wait_signal -> on_the_path
 -- TODO add "good journey" accouncement for state change manual_control(+speed=0) -> on_the_path
 -- TODO add "random announcements": min. distance to stations, min distance between stations, every X rails?, better would be time-based
